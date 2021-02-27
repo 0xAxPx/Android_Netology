7baa0bedd3cfa1d63462b40ac417610ace034d00
Alexander Peshkov <alexander.peshkov27@gmail.com>
2021-02-27 19:00:23 +0300

    task2: add like,share and view material elements

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -9,24 +9,27 @@ import ru.netology.nmedia.util.Util
 
 class MainActivity : AppCompatActivity() {
 
-    inline fun <reified T> Gson.fromJson(json: String) =
-        fromJson<T>(json, object : TypeToken<T>() {}.type)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val jsonToString = Util.readFileFromAssets(context = applicationContext, fileName = POSTS_FILE)
         if (jsonToString != null) {
             val posts = Util.getJsonDataByAuthor(json2String = jsonToString, author = AUTHOR)[0]
-//            val posts: PostData = Gson().fromJson<PostData>(jsonToString)
-//            logger.info("Reading $POSTS_FILE from assets")
-//            val data = posts.data.get(0)
             val author = findViewById<TextView>(R.id.author)
             author.text = posts.author
             val published = findViewById<TextView>(R.id.published)
             published.text = posts.published
             val content = findViewById<TextView>(R.id.content)
-            content.setText(posts.content)
+            content.text = posts.content
+
+            //temp setting count for likes, share and views
+            val likesCount = findViewById<TextView>(R.id.likeCount)
+            likesCount.text = "33"
+            val shareCount = findViewById<TextView>(R.id.shareCount)
+            shareCount.text = "44"
+            val viewsCount = findViewById<TextView>(R.id.viewsCount)
+            viewsCount.text = "111"
+
         }
     }
 
