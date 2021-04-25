b27e79e91e40492f77a14be46b98cf2bb8051477
Alexander Peshkov <alexander.peshkov27@gmail.com>
2021-04-25 20:16:42 +0300

    task13: fix

--- app/src/main/java/com/peshale/nmedia/service/FcmService.kt
+++ app/src/main/java/com/peshale/nmedia/service/FcmService.kt
@@ -2,19 +2,14 @@ package com.peshale.nmedia.service
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import android.os.Build
-import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.app.TaskStackBuilder
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import com.peshale.nmedia.R
-import com.peshale.nmedia.activity.AppActivity
 import com.peshale.nmedia.utils.Arguments.RANDOM_INT
 import kotlin.random.Random
 
@@ -39,14 +34,13 @@ class FcmService : FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
-
         message.data[action]?.let {
             when (it) {
-                "LIKE" -> handleLike(gson.fromJson(message.data[content], Like::class.java))
-                "SHARE" -> handleShare(gson.fromJson(message.data[content], Share::class.java))
-                "VIEW" -> handleView(gson.fromJson(message.data[content], View::class.java))
-                "NEW_POST" -> handleNewPost(gson.fromJson(message.data[content], NewPost::class.java))
-                else -> handleElse(gson.fromJson(message.data[content], Else::class.java))
+                "LIKE" -> handleNotification(gson.fromJson(message.data[content], Notification::class.java), R.string.notification_user_liked)
+                "SHARE" -> handleNotification(gson.fromJson(message.data[content], Notification::class.java), R.string.notification_user_share)
+                "VIEW" -> handleNotification(gson.fromJson(message.data[content], Notification::class.java), R.string.notification_new_viewer)
+                "NEW_POST" -> handleNotification(gson.fromJson(message.data[content], Notification::class.java), R.string.notification_new_post)
+                else -> handleNotification(gson.fromJson(message.data[content], Notification::class.java), R.string.notification_else_action)
             }
         }
     }
@@ -55,92 +49,14 @@ class FcmService : FirebaseMessagingService() {
         println("$token")
     }
 
-    private fun handleNewPost(content: NewPost) {
-        val remoteViews = RemoteViews(packageName, R.layout.notification)
-        remoteViews.setTextViewText(
-            R.id.notificationTitle,
-            getString(
-                R.string.notification_new_post,
-                content.userName
-            )
-        )
-        remoteViews.setTextViewText(R.id.notificationText, content.text)
-        val resultIntent = Intent(this, AppActivity::class.java)
-
-        val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
-            addNextIntentWithParentStack(resultIntent)
-            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
-        }
-
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setContentIntent(resultPendingIntent)
-            .setSmallIcon(R.drawable.netology)
-            .setCustomContentView(remoteViews)
-            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .build()
-
-        NotificationManagerCompat.from(this)
-            .notify(Random.nextInt(RANDOM_INT), notification)
-    }
-
-    private fun handleLike(content: Like) {
+    private fun handleNotification(content: Notification, text: Int) {
         val notification = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.netology)
             .setContentTitle(
                 getString(
-                    R.string.notification_user_liked,
+                    text,
                     content.userName,
-                    content.postAuthor,
-                )
-            )
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .build()
-
-        NotificationManagerCompat.from(this)
-            .notify(Random.nextInt(RANDOM_INT), notification)
-    }
-
-    private fun handleShare(content: Share) {
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.netology)
-            .setContentTitle(
-                getString(
-                    R.string.notification_user_share,
-                    content.userName,
-                    content.postAuthor,
-                )
-            )
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .build()
-
-        NotificationManagerCompat.from(this)
-            .notify(Random.nextInt(RANDOM_INT), notification)
-    }
-
-    private fun handleView(content: View) {
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.netology)
-            .setContentTitle(
-                getString(
-                    R.string.notification_new_viewer,
-                    content.postAuthor,
-                )
-            )
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .build()
-
-        NotificationManagerCompat.from(this)
-            .notify(Random.nextInt(RANDOM_INT), notification)
-    }
-
-    private fun handleElse(content: Else) {
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.netology)
-            .setContentTitle(
-                getString(
-                    R.string.notification_else_action,
-                    content.postAuthor
+                    content.postId
                 )
             )
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
@@ -151,37 +67,10 @@ class FcmService : FirebaseMessagingService() {
     }
 }
 
-data class NewPost(
-    val id: Long,
-    val userName: String,
-    val author: String,
-    val text: String,
-)
-
-data class Like(
+data class Notification(
     val userId: Long,
     val userName: String,
     val postId: Long,
     val postAuthor: String,
-)
-
-data class Share(
-    val userId: Long,
-    val userName: String,
-    val postId: Long,
-    val postAuthor: String,
-)
-
-data class View(
-    val userId: Long,
-    val userName: String,
-    val postId: Long,
-    val postAuthor: String,
-)
-
-data class Else(
-    val userId: Long,
-    val userName: String,
-    val postId: Long,
-    val postAuthor: String,
+    val text: String
 )
